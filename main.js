3366049313ae74d5fac3dfba71e37d457a00d4b5
sjhuang26 <sjhuang26@gmail.com>
2018-04-21 16:25:18 -0400

    Changed to custom gradient backgrounds

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-// Change this to change the number of quotes
-var QUOTE_NUM=3;
-var QUOTE_URLS=["quote-1.png","quote-2.jpg","quote-3.png"];
-
-var idx=Math.floor(Math.random()*QUOTE_NUM);
-
-document.getElementById("main-image-wrap").style.backgroundImage="url('"+QUOTE_URLS[idx]+"')";
+function rand(low, high) {
+  return low+Math.floor(Math.random()*(high-low+1));
+}
+function makeHSL(color){
+  return "hsl("+color+","+rand(60,100)+"%,"+rand(30,70)+"%)";
+}
+var unfiltered=RAW_QUOTE_STRING.split(/\n/);
+var quotes=[];
+for(var i=0;i<unfiltered.length;++i){
+  unfiltered[i]=unfiltered[i].trim();
+  if(unfiltered[i].length != 0)quotes.push(unfiltered[i]);
+}
+function makeQuote(){
+  var color1 = rand(0,359);
+  var color2 = color1 + rand(40,120);
+  var randQuote = quotes[rand(0,quotes.length - 1)];
+  document.getElementById("app").style.background="linear-gradient(90deg, "+makeHSL(color1)+" 0%,"+makeHSL(color2)+" 100%)";
+  document.getElementById("quote").textContent = randQuote;
+  document.getElementById("search").href="https://www.google.com/search?q="+encodeURI(randQuote);
+}
+makeQuote();
+document.getElementById("new-quote").addEventListener("click", function(){
+  makeQuote();
+});
+document.getElementById("credits").addEventListener("click", function(){
+  document.getElementById("quote").textContent = "Made by Suhao Jeffrey Huang. Quotes from various online sources.";
+});
---
+++ quote-data.js
@@ -0,0 +1,37 @@
+/*
+  This is the list of quotes used by this program.
+  Put one (and only one) quote on each line.
+  Don't put quotes outside of the tick marks (` `).
+*/
+var RAW_QUOTE_STRING = `
+Those who matter don’t mind, and those that mind don’t matter.
+Be the energy you want to attract.
+Difficult roads lead to beautiful destinations.
+When you can’t find the sunshine, be the sunshine.
+Life is a series of a thousand tiny miracles. Notice and cherish them.
+A winner is just a loser that tried one more time.
+Wake up each morning with the thought that something wonderful is about to happen.
+Learn from yesterday, live for today and hope for tomorrow.
+Strive for progress, not perfection.
+Keep your face to the sunshine and you cannot see a shadow.
+Nothing can dim the light that shines from within.
+Start each day with a grateful heart.
+When in doubt, be kind.
+Everyone shines, given the right lighting.
+Love life and it will love you right back.
+With the new day comes new strength and new thoughts.
+You never know when the best day of your life is going to be.
+Sometimes when things are falling apart, they may actually be falling into place.
+When life takes away, something of greater value is always given in return.
+The one who falls and gets up is so much stronger than the one who never fell.
+We may encounter many defeats but we must not be defeated.
+Believe you can and you’re halfway there.
+Yesterday is not ours to recover, but tomorrow is ours to win or lose.
+Turn your face to the sun and the shadow falls behind you.
+The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.
+Practice like you’ve never won; perform like you’ve never lost.
+If you can’t stop thinking about it, don’t stop working for it.
+Whether you think you can or you think you can’t, you’re right.
+Perfection is not attainable, but if we chase perfection we can catch excellence.
+Life is 10% what happens to me and 90% of how I react to it.
+`;
